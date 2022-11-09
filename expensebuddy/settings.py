b20c0a592048407f444e7ce78521111626f310f6
pranavchougule511 <32321752+pranavchougule511@users.noreply.github.com>
2022-11-09 20:25:12 +0530

    Update settings.py

--- expensebuddy/settings.py
+++ expensebuddy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-el(n975azm2s1uoj3$bub!l)fl)b1gjwno=g75*w!l$zxbnju!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['expensebuddyz.herokuapp.com', '127.0.0.1']
 
